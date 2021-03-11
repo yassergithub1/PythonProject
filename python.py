c24faba026f44ab2147006bdc835914f9dde6d9a
yazedgithub1 <yazedgithub1@gmail.com>
2021-03-11 21:21:06 +0000

    added task c and d

---
+++ python.py
@@ -0,0 +1,35 @@
+# c) Write a program containing a function to convert the current time, into military time. 
+
+import datetime
+def timeconvert(str1):
+    if str1[-2:] == "AM" and str1[:2] == "12":
+        return "00" + str1[2:-2]
+    elif str1[-2:] == "AM":
+        return str1[:-2]
+    elif str1[-2:] == "PM" and str1[:2] == "12":
+        return str1[:-2]
+    else:
+        return str(int(str1[:2]) + 12) + str1[2:8]
+dt=datetime.datetime.now()
+print("Conversion Of Time ::",timeconvert(dt.strftime("%H:%M:%S")))
+
+# d) Write a program containing a function to output the fibonacci sum up to a user inputted number.
+
+nterms = int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+
+if nterms <= 0:
+    print("Please enter a positive integer")
+elif nterms == 1:
+    print("Fibonacci sequence upto",nterms,":")
+    print(n1)
+else:
+    print("Fibonacci sequence:")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
+
